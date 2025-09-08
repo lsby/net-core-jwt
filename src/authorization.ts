8746db7cb8638714de3abbeda5b7d04b23744672
hbybyyang <2451759073@qq.com>
2025-09-08 19:20:41 +0800

    区分authorization和token

--- src/authorization.ts
+++ src/authorization.ts
@@ -2,14 +2,14 @@ import { z } from 'zod'
 import { 插件 } from '@lsby/net-core'
 
 let zod类型表示 = z.object({
-  token: z.string().or(z.undefined()),
+  authorization: z.string().or(z.undefined()),
 })
 
-export class GetToken插件 extends 插件<typeof zod类型表示> {
+export class GetAuthorization插件 extends 插件<typeof zod类型表示> {
   constructor() {
     super(zod类型表示, async (req) => {
       return {
-        token: req.headers.authorization?.replace('Bearer ', ''),
+        authorization: req.headers.authorization,
       }
     })
   }
